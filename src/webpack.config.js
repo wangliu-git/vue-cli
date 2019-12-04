4836ecd54c84b5da6416165cff0ad5f3a7933df8
wangliu <1947389483@qq.com>
2019-12-04 18:47:13 +0800

    sdfgh

---
+++ src/webpack.config.js
@@ -0,0 +1,31 @@
+const path = reqire('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  //模式:生产环境
+  mode : 'prodution',
+
+  //入口
+  entry : {
+
+  },
+
+  //出口
+  output : {
+
+  },
+
+  //模块加载器
+  module : {
+    
+    rule : [
+
+    ]
+  },
+
+
+  //插件
+  plugins : [
+
+  ]
+}
