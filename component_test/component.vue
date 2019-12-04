fbb158d595705a555d601542a1b68aab348be956
wangliu <1947389483@qq.com>
2019-12-04 20:16:58 +0800

    b

---
+++ component_test/component.vue
@@ -0,0 +1,26 @@
+<template>
+<!-- 模板页面 -->
+  <div>
+    {{hello }} vue 
+  </div>
+</template>
+
+
+// js代码块
+<script>
+export default {
+  data:function() {
+    return {
+      hello:"hi"
+    }
+  }
+}
+</script>
+
+
+//样式
+<style lang="stylus" scoped>
+  div{
+    background-color pink
+  }
+</style>
