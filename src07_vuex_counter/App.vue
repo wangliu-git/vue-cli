9dad4715c0030b17a0801c9687395c1575d99592
wangliu <1947389483@qq.com>
2019-12-11 00:05:22 +0800

    ad

--- src07_vuex_counter/App.vue
+++ src07_vuex_counter/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     App
+       <!-- <p>click {{$store.state.count}} times, count is {{$store.state.evenOrOdd}}</p> -->
    <p>click {{count}} times, count is {{evenOrOdd}}</p>
    <button @click="increment">+</button>
    <button @click="decrement">-</button>
@@ -19,11 +20,27 @@
     4. commit(mutationName, data)
 
   */
+
+/*
+    map : 映射 对应关系  key  value
+读数据
+    mapState: (函数类型)    映射状态
+    mapGetters:(函数类型)   getter数据
+更新数据
+    mapMutations:   commit
+    mapActions:  dispath
+*/
+
+
+// 用于简化组件通信的函数
   import {mapState, mapGetters, mapMutations, mapActions} from 'vuex'
 
   export default {
 
+
+//计算属性的名字和状态的名字一致
     computed: {
+
       // count () {
       //   return this.$store.state.count
       // },
@@ -32,7 +49,10 @@
       //   return this.$store.getters.evenOrOdd
       // }
 
-      ...mapState(['count']), // {count () {return this.$store.state['count']}}
+
+//函数-接受的是数组--返回值是(...写在对象里) --对象   
+  ...mapState(['count']), // {count () {return this.$store.state['count']}}
+
       ...mapGetters(['evenOrOdd']) // {evenOrOdd () {return this.$store.getters['evenOrOdd']}}
     },
     
@@ -42,6 +62,10 @@
     
 
     methods: {
+
+
+//commit让nmutation调用的  mapMutations简化
+
       // increment () {
       //   this.$store.commit('INCREMENT')
       // },
@@ -49,6 +73,9 @@
       // decrement () {
       //   this.$store.commit('DECREMENT')
       // },
+
+
+//分发-mapActions简化
 
       // incrementIfOdd () {
       //   this.$store.dispatch('incrementIfOdd')
@@ -60,6 +87,8 @@
 
       ...mapActions(['incrementIfOdd', 'incrementAsync']),
       ...mapMutations({
+      
+    //由于不同的名字,使名字相对应
         increment: 'INCREMENT',
         decrement: 'DECREMENT'
       }),
